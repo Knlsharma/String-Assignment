97e92cff1f193b9c90bd524dd680aa615a7191e9
Kunal Sharma <knlsharma678@gmail.com>
2018-09-10 16:42:02 +0530

    started

---
+++ countspace.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class countspace 
+{
+    public static void main (String args[])
+    {int i,sc=0;
+Scanner s= new Scanner(System.in);
+String str=s.nextLine();
+            for(i=0;i<str.length();i++)
+            {
+ 
+    if( str.charAt(i)==' ')  {       
+     sc++;
+System.out.println("Total space are" + s+ " index at"+ (i+1));
+            }
+else{
+
+}
+
+           
+}
+}
+    }
+         
---
+++ wordcount.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class wordcount 
+{
+public static void main ( String args[])
+{
+Scanner x=new Scanner(System.in);
+int c=0;
+String str=x.nextLine();
+for(int i=0;i<str.length();i++)
+{
+c=c+1;
+}
+ 
+}
+System.out.println("total count is " + c);
+}
+
