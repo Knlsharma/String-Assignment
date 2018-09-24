cdf92ebe5c0ee39a32f87df0fd3cd1702af7f35a
Kunal Sharma <knlsharma678@gmail.com>
2018-09-24 15:04:39 +0530

    added

---
+++ BinaryToDecimal.java
@@ -0,0 +1,41 @@
+/*  
+import java.util.Scanner;
+class BinaryToDecimal
+{
+        public static void main(String args[])
+        {
+            Scanner s=new Scanner(System.in);
+            
+            System.out.println("Enter a binary number:");
+ 
+            String n=s.nextLine();
+            
+            System.out.println(Integer.parseInt(n,6));
+        }
+}
+
+*/
+import java.util.Scanner;
+ 
+class BinaryToDecimal
+{
+        public static void main(String args[])
+        {
+            Scanner s=new Scanner(System.in);
+            
+            System.out.println("Enter a binary number:");
+            int n=s.nextInt();
+            
+            int decimal=0,p=0;
+            
+            while(n!=0)
+            {
+                decimal+=((n%10)*Math.pow(2,p));
+                n=n/10;
+                p++;
+            }
+            
+            System.out.println(decimal);
+        }
+}
+ 
